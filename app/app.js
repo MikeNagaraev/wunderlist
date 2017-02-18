3a1a400480c07e2ddca9b3aa94f343e801030352
MikhailLU <clowfinger13@gmail.com>
2017-02-18 15:55:01 +0300

    directives

--- app/app.js
+++ app/app.js
@@ -4,14 +4,11 @@ import homeController from './components/home/homeController';
 // import todosController from './components/todos/todosController';
 import categoriesController from './components/categories/categoriesController'
 
-// import $ from 'jquery';
-
 import categoriesService from './components/categories/categoriesService';
 import todosService from './components/todos/todosService';
 
 import mainConfig from './config';
 import style from './assets/stylesheets/main.scss';
-window.$ = $;
 // import application from './assets/javascripts/application';
 
 const app = angular.module('wunderlist', [angularRouter])
@@ -33,3 +30,78 @@ const app = angular.module('wunderlist', [angularRouter])
   .service('todosService', ['$http', function($http) {
     return new todosService($http);
   }])
+  .directive("selectableDirective", function() {
+    return {
+      controller: ($scope, $element) => {
+        $scope.selectCategory = () => {
+          $scope.resetSelectedCategories();
+          if ($element.hasClass('category-item')) {
+            if ($element.hasClass('selected')) {
+              return;
+            } else {
+              $element.addClass('selected');
+            }
+          }
+        }
+        $scope.resetSelectedCategories = () => {
+          $('.category-item').removeClass('selected');
+        }
+
+        $element.click($scope.selectCategory);
+      }
+    }
+  })
+  .directive("modalShow", function() {
+    return {
+      controller: ($scope, $element) => {
+        $scope.showCategoryWindow = () => {
+          if (!$('#modal-category').hasClass('opened')) {
+            $('#modal-category').addClass('opened').show();
+          }
+        }
+        $element.click($scope.showCategoryWindow)
+      }
+    }
+  })
+  .directive("modalHide", function() {
+    return {
+      controller: ($scope, $element) => {
+        $scope.hideCategoryWindow = () => {
+          if ($('#modal-category').hasClass('opened')) {
+            $('#modal-category').removeClass('opened').hide();
+          }
+        }
+        $element.click($scope.hideCategoryWindow);
+      }
+    }
+  })
+  .directive("toggleDirective", function() {
+    return {
+      controller: ($scope, $element) => {
+        $scope.toggleAside = () => {
+          if ($element.hasClass('opened')) {
+            $('.home-aside').css('width', '50px');
+            $element.removeClass('opened');
+            $scope.toggleHomeContainer(false);
+          } else {
+            $element.addClass('opened');
+            $('.home-aside').css('width', '15%');
+            $scope.toggleHomeContainer(true);
+          }
+        }
+
+        $element.click($scope.toggleAside);
+
+        $scope.toggleHomeContainer = flag => {
+          if (flag) {
+            $('.home-container').css('width', '85%');
+          } else {
+            $('.home-container').css('width', 'calc(100% - 50px)');
+          }
+        }
+
+      }
+    }
+
+
+  })
